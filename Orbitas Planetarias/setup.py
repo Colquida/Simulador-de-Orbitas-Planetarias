ca8675c80e8d639277184c8cd6d9b37765877249
Jonathan Alexander Torres Benítez <83324055+Colquida@users.noreply.github.com>
2021-05-14 21:27:04 -0500

    Add files via upload

---
+++ Orbitas Planetarias/setup.py
@@ -0,0 +1,22 @@
+#from distutils.core import setup, Extension
+#from Cython.Build import cythonize
+
+#exts = (cythonize("Cy_simulator.pyx"))
+
+#setup(ext_modules=exts)
+from distutils.core import setup
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+from distutils.extension import Extension
+
+
+#exts = (cythonize('Cy_functionE.pyx'))
+ext_modules=[ Extension("Cy_simulator",
+              ["Cy_simulator.pyx"],
+              libraries=["m"],
+              extra_compile_args=["-O3", "-ffast-math", "-march=native"])]
+
+setup(name = 'Cy_simulator', cmdclass={"build_ext": build_ext},
+	ext_modules=ext_modules)
+
+
